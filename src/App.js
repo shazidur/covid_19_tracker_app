ddbe911c5c9de2ea9f3ff8708534c81d1f67409f
shazidur <jadushazid007@ggmail.com>
2020-09-24 15:53:48 +0900

    complited the mockup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { MenuItem, FormControl, Select } from "@material-ui/core";
+import { MenuItem, FormControl, Select, Card } from "@material-ui/core";
 
+import InfoBox from "./InfoBox";
+import Map from "./Map";
 import "./App.css";
 
 function App() {
@@ -33,30 +35,39 @@ function App() {
 
   return (
     <div className="app">
-      <div className="app__header">
-        <h1> Covid-19 Tracker </h1>
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" value={country} onClick={onCountryChange}>
-            <MenuItem value={"Worldwide"}> Worldwide </MenuItem>
+      <div className="app__left">
+        <div className="app__header">
+          <h1> Covid-19 Tracker </h1>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onClick={onCountryChange}
+            >
+              <MenuItem value={"Worldwide"}> Worldwide </MenuItem>
 
-            {countries.map((country) => (
-              <MenuItem value={country.value}> {country.name} </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+              {countries.map((country) => (
+                <MenuItem value={country.value}> {country.name} </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__status">
+          <InfoBox
+            title="Coronavirus cases"
+            cases={23333}
+            total={200}
+          ></InfoBox>
+          <InfoBox title="Recoverd" cases={3323} total={400}></InfoBox>
+          <InfoBox title="Deaths" cases={20} total={30}></InfoBox>
+        </div>
+        <Map />
       </div>
-
-      {/* Header */}
-      {/* Title - Select input dropdown field */}
-
-      {/*  infobox1 */}
-      {/*  infobox 2*/}
-      {/*  infobox 3*/}
-
-      {/* Table */}
-      {/* Graph */}
-
-      {/* Map */}
+      <Card className="app__right">
+        <h3> Live cases by country </h3>
+        <h3> Worldwide new cases </h3>
+      </Card>
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Map() {
+  return (
+    <div>
+      <p>I am Map </p>
+    </div>
+  );
+}
+
+export default Map;
