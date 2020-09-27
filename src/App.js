15c067094ec8f2463aa94186f8e1b7f31531e8a0
shazidur <jadushazid007@ggmail.com>
2020-09-27 16:08:04 +0900

    add and active map component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ import Table from "./Table";
 import LineGraph from "./LineGraph";
 import { sortData } from "./util";
 import "./App.css";
+import "leaflet/dist/leaflet.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+  const [mapCountries, setMapCountries] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -35,6 +39,7 @@ function App() {
           setCountries(countries);
           const sortedData = sortData(data);
           setTableData(sortedData);
+          setMapCountries(data);
         });
     };
     getCountryData();
@@ -54,6 +59,8 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         setCountryInfo(data);
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long]);
+        setMapZoom(4);
       });
   };
 
@@ -93,7 +100,7 @@ function App() {
             total={countryInfo.deaths}
           ></InfoBox>
         </div>
-        <Map />
+        <Map countries={mapCountries} center={mapCenter} zoom={mapZoom} />
       </div>
       <Card className="app__right">
         <h3> Live cases by country </h3>
--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Map.css";
+import { Map as LeafLetMap, TileLayer } from "react-leaflet";
+import { showDataForMap } from "./util";
 
-function Map() {
+function Map({ countries, casesType, center, zoom }) {
   return (
-    <div>
-      <p>I am Map </p>
+    <div className="map">
+      <LeafLetMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {/* make a loop for map point */}
+
+        {showDataForMap(countries, casesType)}
+      </LeafLetMap>
     </div>
   );
 }
